628c3e79fb6843ab4f058a1695c8cd54650b41cb
andre <andrecabralpires@gmail.com>
2022-11-15 11:13:11 +0000

    final lecture

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: "restaurant_edit"
+  # patch "restaurants/:id", to: "restaurants#update"
+  # delete "restaurants/:id", to: "restaurants#destroy"
 end
