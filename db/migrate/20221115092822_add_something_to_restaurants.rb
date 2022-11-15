ef6c3b04f3d7ace83c10792f4ee0e023b381ce8f
andre <andrecabralpires@gmail.com>
2022-11-15 09:33:27 +0000

    migrations

---
+++ db/migrate/20221115092822_add_something_to_restaurants.rb
@@ -0,0 +1,9 @@
+class AddSomethingToRestaurants < ActiveRecord::Migration[7.0]
+  def change
+    # add_column --> add new column to table
+    # change_column --> changes the type of the column (integer,string..)
+    # rename_column --> change the name of the column
+    # remove_column --> remove the column
+    # add_reference --> adds a foreign key to the table referencing other tablez
+  end
+end
